f838dedf6cd7b7e512b1d457f6ea7d2bd1899f6f
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-03-01 23:39:57 +0100

    ability to perform multiple operations

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ There shall be a section for normal calculations and another for simple scientif
 
 ## Plan
 
-Our calculator will have a very simple user interface 
+Our calculator will have a very simple user interface
 Users can select the operands and operators with click events,
 and The desired ouput will be the result of the operation displayed on the screen
 
@@ -16,3 +16,7 @@ and The desired ouput will be the result of the operation displayed on the scree
 
 The algorithm in Pseudocode for this problem includes:
 when a user clicks a operator and operand, it is stored. however, the equals button signals for the operation to be done, and its result is displayed to the screen when a user clicks the clear button, varibles containing all initial values are clear to re-begin
+
+## Problem Encountered
+
+I was stuck with the inability to perform multiple operations at the same time. It took me for days.
--- js/script.js
+++ js/script.js
@@ -36,21 +36,46 @@ const operate = function (number1, operator, number2) {
   }
 };
 
-let firstValue = false;
-let displayValue = "";
-let firstNumber;
-let secondNumber;
+let displayValue = ""; //value to be displayed on screen
+let firstNumber = ""; //first number inputed before calculator
+let secondNumber = ""; //first number inputed after operator
 let operator = "";
+let answer;
+let firstValue; //converted firstNumber( from string to number )
+let secondValue; //converted secondNumber( from string to number )
 
 const storeNumbers = function (e) {
+  /* if there is no operator, the number would be recorded in the first Number string. 
+  However, if there is already an operator, the numbers would be saved in second number string */
+  if (operator === "") {
+    firstNumber += e.target.textContent;
+  } else {
+    secondNumber += e.target.textContent;
+  }
   displayValue += e.target.textContent;
   inputScreen.textContent = displayValue;
 };
 
 const storeOperations = function (e) {
-  firstNumber = parseFloat(displayValue);
-  operator = e.target.textContent;
-  displayValue = "";
+  /* if there is no operator, the operator would be saved. However, mostly for multiple operations, 
+  if there is already an operator, there is a need to get the first number, second number 
+  and perform the needed operation, then the new answer is given back as the new first number, 
+  the original operator is removed, the initial second number is also removed, 
+  and the newly clicked operator is given as the new operator   */
+  if (operator === "") {
+    operator = e.target.textContent;
+  }
+  // for multiple steps mostly
+  else {
+    firstValue = parseFloat(firstNumber);
+    secondValue = parseFloat(secondNumber);
+    answer = operate(firstValue, operator, secondValue);
+    firstNumber = answer;
+    operator = "";
+    secondNumber = "";
+    operator = e.target.textContent;
+  }
+  displayValue += operator;
 };
 
 const displayValuesOnScreen = function () {
@@ -66,8 +91,9 @@ const displayValuesOnScreen = function () {
 displayValuesOnScreen();
 
 equalsBtn.addEventListener("click", function () {
-  secondNumber = parseFloat(displayValue);
-  const answer = operate(firstNumber, operator, secondNumber);
-  displayValue = answer;
+  firstValue = parseFloat(firstNumber);
+  secondValue = parseFloat(secondNumber);
+  console.log(firstValue, operator, secondValue);
+  answer = operate(firstValue, operator, secondValue);
   answerScreen.textContent = answer;
 });
